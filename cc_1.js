668a2772a3587ee4b248320acaed4f52aa048ed9
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-01 16:20:24 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Brandon Mejia";
+const employeeID = 250;
+var isActive = false;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
