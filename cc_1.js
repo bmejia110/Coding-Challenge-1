c143011af0d00920732655b298d81ebe48c1abdd
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-01 19:41:29 -0400

    Task 3: Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -20,3 +20,20 @@ const productDetails = {
 }
 
 console.log(productNames, productDetails);
+
+// Task 3: Assignment Operators
+
+let accountBalance = 1000;
+console.log('Initial Balance: $${accountBalance}' );
+accountBalance += 500;
+console.log('After deposit of $500: $${accountBalance}' );
+accountBalance -= 200;
+console.log(' After withdrawl of $200: $${account Balance}');
+accountBalance *= 1.05;
+console.log('After applying 5% interest: $${accountBalance.toFixed(2)}' );
+accountBalance /= 2;
+console.log('After halving due to service fee: $${accountBalance.toFixed(2)}' );
+accountBalance %= 300;
+console.log('After applying modulo operation with 300: $${accountBalance.toFixed(2)}');
+
+
