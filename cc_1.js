f2cba892fd0f3c7a1adae1e8a0f03e92e4a093c5
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-01 19:25:31 -0400

    Task 2: Compound Data Types

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,16 @@ var isActive = false;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compound Data Types
+
+let products = ["apple", "orange", "Banana"];
+const productDetails = {
+    name: productNames, 
+    price: 3.75
+    inStock: true
+}
+
+console.log(productNames, productDetails);
