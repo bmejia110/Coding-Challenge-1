b93c1bc9ecee9b95f9efc5dd6fb49ef308331d7c
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-01 20:24:15 -0400

    Task 4 - Employee Performance Evaluation

--- cc_1.js
+++ cc_1.js
@@ -26,14 +26,26 @@ console.log(productNames, productDetails);
 let accountBalance = 1000;
 console.log('Initial Balance: $${accountBalance}' );
 accountBalance += 500;
-console.log('After deposit of $500: $${accountBalance}' );
+console.log(accountBalance);
 accountBalance -= 200;
-console.log(' After withdrawl of $200: $${account Balance}');
+console.log(accountBalance);
 accountBalance *= 1.05;
-console.log('After applying 5% interest: $${accountBalance.toFixed(2)}' );
+console.log(accountBalance.toFixed(2));
 accountBalance /= 2;
-console.log('After halving due to service fee: $${accountBalance.toFixed(2)}' );
+console.log(accountBalance.toFixed(2));
 accountBalance %= 300;
-console.log('After applying modulo operation with 300: $${accountBalance.toFixed(2)}');
+console.log(accountBalance.toFixed(2));
+
+
+// Task 4: Comparison Operators
+
+let employeeScore1 = 85;
+let employeeScore2 = 92;
+console.log(employeeScore1 > employeeScore2);
+console.log(employeeScore1 < employeeScore2);
+console.log(employeeScore1 >= employeeScore2);
+console.log(employeeScore1 <= employeeScore2);
+console.log(employeeScore1 === employeeScore2);
+console.log(employeeScore1 !== employeeScore2);
 
 
